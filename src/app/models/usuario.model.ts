e2ffdf68b54d5dfbb4eb777b3ef65e7aa69d8620
Alejandro U Almazan <alejandro.orlova@gmail.com>
2020-02-26 15:27:19 -0600

    Seccion 12 terminada - efectos

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,13 @@
+export class Usuario {
+    // public Id: number;
+    // public FirstName: string;
+    // public LastName: string;
+    // public Avatar: string;
+
+    constructor(
+        public id: number,
+        public firstName: string,
+        public lastName: string,
+        public avatar: string
+    ) {}
+}
---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: []
+})
+export class ListaComponent implements OnInit {
+
+  usuarios: Usuario[] = [];
+
+  constructor(
+    public usuarioService: UsuarioService
+  ) { }
+
+  ngOnInit() {
+    this.usuarioService.getUsers().subscribe( users => {
+      console.log(users);
+      this.usuarios = users;
+    });
+  }
+
+}
